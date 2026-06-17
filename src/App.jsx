f9319ac023a2f5fc0b3ca7d89ee82070e319766d
nehamdass1-hash <nehamdass1@gmail.com>
2026-06-17 09:12:26 +0530

    react learning

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sample from './components/Sample'
+import New from './components/New'
+import Childcomp from './components/Childcomp'
+import ParentComp from './components/ParentComp'
+import Counter from './components/Counter'
+
+const App = () => {
+  return (
+    <>
+<Counter></Counter>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/New.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const New = () => {
+  return (
+  <>
+  <div>
+    <h1>Hehehehe</h1>
+  </div>
+  </>
+  )
+}
+
+export default New
